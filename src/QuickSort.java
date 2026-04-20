2eb38398411b6de9c0c7f0fcd3ca891f3b041bc7
Nico_ADU <nicolasbetancur08@gmail.com>
2026-04-20 11:33:40 -0500

    Quick y Merge

---
+++ src/QuickSort.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class QuickSort {
+
+    //Escoger pivote de manera aleatoria
+    private static final Random random = new Random();
+
+    public static<T extends Comparable<T>> void quickSort(T[] arr){
+
+    }
+
+    private static<T extends Comparable<T>> void ordenar(T[] arr, int bajo, int alto){
+
+    }
+
+    private static<T extends Comparable<T>> void particionar(T[] arr, int bajo, int alto){
+
+    }
+
+    private static<T extends Comparable<T>> void intercambiar(T[] arr, int a, int b){
+
+    }
+
+
+}
